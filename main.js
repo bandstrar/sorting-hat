28caf145ea8acea9af45bda61e725985dafb20fb
bandstrar <rob.bandstra@gmail.com>
2020-08-01 17:09:31 -0500

    added working expel button

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const buildForm = () => {
     let domString = '';
 
     domString += `<h2 id="firstYear">Enter First Year's Name</h2>`
-    domString += `<form class="form-inline">
+    domString += `<div class="form-inline">
     <div class="form-group mb-2">
       <label for="staticStudent" class="sr-only">Student</label>
       <input type="text" readonly class="form-control-plaintext" id="staticStudent" value="Student:">
@@ -28,12 +28,9 @@ const buildForm = () => {
       <input type="text" class="form-control" id="inputStudent" placeholder="Ron Weasley">
     </div>
     <button type="submit" class="btn btn-primary mb-2" id="formBtn">Sort!</button>
-  </form>`
+  </div>`
 
   printToDom('sortForm', domString);
-
-  document.querySelector('#formBtn').addEventListener('click', buildStudent)
-  document.querySelector('#formBtn').addEventListener('click', buildCards)
 }
 
 const buildStudent = (e) => {
@@ -41,33 +38,44 @@ const buildStudent = (e) => {
   let randomHouse = houseArray[Math.floor(Math.random() * houseArray.length)];
 
   if (buttonId === 'formBtn') {
-    studentArray.push({name: `${inputStudent.value}`, house: `${randomHouse}`})
+    studentArray.push({name: inputStudent.value, house: randomHouse})
   }
+  buildCards(studentArray);
 }
 
-const buildCards = (e) => {
-    let buttonId = e.target.id;
+const buildCards = (arrayGrabber) => {
     let domString = ''
     
-    if (buttonId === 'formBtn'){
-      for (let i = 0; i < studentArray.length; i++) {
-    domString += `<div class="card" style="width: 18rem;">
+      for (let i = 0; i < arrayGrabber.length; i++) {
+    domString += `<div class="card" style="width: 18rem;" id="${i}">
     <div class="card-body">
-    <h5 class="card-name">${studentArray[i].name}</h5>
-    <p class="card-text">${studentArray[i].house}</p>
-    <a href="#" class="btn btn-primary">Expel</a>
+    <h5 class="card-name">${arrayGrabber[i].name}</h5>
+    <p class="card-text">${arrayGrabber[i].house}</p>
+    <button type="button" class="btn btn-primary" id="${i}">Expel</button>
   </div>
 </div>`
-    }
   }
 
 printToDom('studentCards', domString)
-
+document.getElementById('inputStudent').value = '';
     }
 
+  const expelStudent = (e) => {
+      const ctype = e.target.type;
+      const target = e.target.id;
+  
+  
+      if (ctype === 'button') {
+          studentArray.splice(target, 1);
+
+          buildCards(studentArray)
+      }
+    }
 
     const buttonEvents = () => {
       document.querySelector('#startBtn').addEventListener('click', buildForm)
+      document.querySelector('#sortForm').addEventListener('click', buildStudent)
+      document.querySelector('#studentCards').addEventListener('click', expelStudent)
     }
 
 
